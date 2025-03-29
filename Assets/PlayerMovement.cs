4c092e220d138dd4596bc587bbfc25f3c65a3a4c
Stefanpng <251063@virtualwindow.co.za>
2025-03-30 00:09:20 +0200

    Jump setup

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -3,15 +3,24 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody2D rb;
+    [Header("Movement")]
     public float moveSpeed = 5f;
 
     float horizontalMovement;
+
+    [Header("Jumping")]
+    public float jumpPower = 10f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         
     }
 
+    [Header("GroundCheck")]
+    public Transform groundCheckPos;
+    public Vector2 groundCheckSize = new Vector2(0.5f, 0.05f);
+    public LayerMask groundLayer;
     // Update is called once per frame
     void Update()
     {
@@ -21,5 +30,24 @@ public class PlayerMovement : MonoBehaviour
     public void Move(InputAction.CallbackContext context)
     {
         horizontalMovement = context.ReadValue<Vector2>().x;
+    }
+
+
+    public void Jump(InputAction.CallbackContext context) 
+    {
+        if(context.performed)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpPower);
+        }
+        else if (context.canceled)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, rb.linearVelocity.y * 0.5f);
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.white;
+        Gizmos.DrawCube(groundCheckPos.position, groundCheckSize);
     }
 }
